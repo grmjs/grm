0d9f3d6f57b80691be0d17d4a2f206fd6b22bfa7
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-14 12:54:09 +0530

    update imports

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,10 @@
 export { Api } from "./tl/api.js";
 export { TelegramClient } from "./client/telegram_client.ts";
-export { Connection } from "./network/mod.ts";
 export { GRAM_BASE_VERSION, VERSION } from "./version.ts";
 export { Logger, LogLevel } from "./extensions/logger.ts";
+
 export * from "./sessions/mod.ts";
+export * from "./network/mod.ts";
 
 // compat
 export * as client from "./client/mod.ts";
@@ -11,7 +12,6 @@ export * as crypto from "./crypto/mod.ts";
 export * as errors from "./errors/mod.ts";
 export * as events from "./events/mod.ts";
 export * as extensions from "./extensions/mod.ts";
-export * from "./network/mod.ts";
 export * as sessions from "./sessions/mod.ts";
 export * as tl from "./tl/mod.ts";
 
--- src/tl/mod.ts
+++ src/tl/mod.ts
@@ -2,4 +2,6 @@ import { patchAll } from "./patcher.ts";
 patchAll();
 
 export { Api } from "./api.js";
+export * from "./core/mod.ts";
+export * from "./custom/mod.ts";
 export * from "./generation_helpers.ts";

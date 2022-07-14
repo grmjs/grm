06389afa1c4938b512d2e520c1f74e70734d6c9a
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-14 12:33:01 +0530

    improve example: deno supports tla

--- examples/simple.ts
+++ examples/simple.ts
@@ -1,27 +1,30 @@
 import { StringSession, TelegramClient } from "../mod.ts";
 
+// Login and create an application on https://my.telegram.org
+// to get values for API ID and API Hash.
 const apiId = 123456;
 const apiHash = "abcd1234";
-// fill this later with the value from session.save()
+
+// Fill in this later with the value from client.session.save(),
+// so you don't have to login each time you run the file.
 const stringSession = new StringSession("");
 
-(async function () {
-  console.log("Loading interactive example...");
-  const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-  });
+console.log("Loading interactive example...");
+const client = new TelegramClient(stringSession, apiId, apiHash, {
+  connectionRetries: 5,
+});
 
-  await client.start({
-    phoneNumber: () => prompt("Enter your phone number:")!,
-    password: async () => await prompt("Enter your password:")!,
-    phoneCode: async () => await prompt("Enter the code you received:")!,
-    onError: (err) => console.log(err),
-  });
+await client.start({
+  phoneNumber: () => prompt("Enter your phone number:")!,
+  password: async () => await prompt("Enter your password:")!,
+  phoneCode: async () => await prompt("Enter the code you received:")!,
+  onError: (err) => console.log(err),
+});
 
-  console.log("You should now be connected.");
-  // Save this string to avoid logging in again
-  console.log(client.session.save());
+console.log("You should now be connected.");
+// Save the output of the following and use it in `new SessionString("")`
+// to avoid logging in again next time.
+console.log(client.session.save());
 
-  // Send a message to yourself
-  await client.sendMessage("me", { message: "Hello!" });
-})();
+// Send a message to yourself
+await client.sendMessage("me", { message: "Hello!" });

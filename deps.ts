74a91fb1d2b4e56aaf83f360799df1b10fe6d87e
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-08-05 13:06:16 +0530

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -5,7 +5,7 @@ export {
   fromFileUrl,
   join,
   resolve,
-} from "https://deno.land/std@0.150.0/path/mod.ts";
+} from "https://deno.land/std@0.151.0/path/mod.ts";
 
 // std/node/
 export {
@@ -13,14 +13,14 @@ export {
   createHash,
   pbkdf2Sync,
   randomBytes,
-} from "https://deno.land/std@0.150.0/node/crypto.ts";
-export { Buffer } from "https://deno.land/std@0.150.0/node/buffer.ts";
+} from "https://deno.land/std@0.151.0/node/crypto.ts";
+export { Buffer } from "https://deno.land/std@0.151.0/node/buffer.ts";
 export {
   createWriteStream,
   existsSync,
   WriteStream,
-} from "https://deno.land/std@0.150.0/node/fs.ts";
-export { Socket } from "https://deno.land/std@0.150.0/node/net.ts";
+} from "https://deno.land/std@0.151.0/node/fs.ts";
+export { Socket } from "https://deno.land/std@0.151.0/node/net.ts";
 
 // x/
 export { SocksClient } from "https://deno.land/x/deno_socks@v2.6.1/mod.ts";
@@ -38,8 +38,10 @@ export {
 } from "https://cdn.skypack.dev/@cryptography/aes@0.1.1?dts";
 export { inflate } from "https://cdn.skypack.dev/pako@v2.0.4?dts";
 export { getExtension, getType } from "https://cdn.skypack.dev/mime?dts";
-export { w3cwebsocket } from "https://esm.sh/websocket@1.0.34";
 export { default as bigInt } from "https://cdn.skypack.dev/big-integer?dts";
+
+// esm.sh/
+export { w3cwebsocket } from "https://esm.sh/websocket@1.0.34";
 
 // ghc.deno.dev/
 export {
